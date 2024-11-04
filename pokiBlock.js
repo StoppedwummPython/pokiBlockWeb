9fbed27c260a6e5c3cb16ff24cf38e4239e8822e
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-11-04 11:10:20 +0100

    Add css

--- pokiBlock.js
+++ pokiBlock.js
@@ -1,5 +1,6 @@
 (async () => {
     const n = await fetch("https://raw.githubusercontent.com/amaterasusan/notification/refs/heads/master/notification.js")
+    const ncss = await fetch("https://raw.githubusercontent.com/amaterasusan/notification/refs/heads/master/notification.css")
     const pokiBlockLiteSrc = `javascript:(async () => {
     PokiSDK.commercialBreak = async function() {
         return {
@@ -13,7 +14,10 @@
             rewardsAllowed: true
         };
     };
-    eval(\`${await n.text()}\`)
+    eval(\`${await n.text()}\`);
+    const ncssTag = document.createElement("style")
+    ncssTag.innerHTML = await \`${await ncss.text()}\`
+    document.head.children[length - 1].after(ncssTag)
     })()`
     document.getElementById("blockLite").href = pokiBlockLiteSrc
     document.getElementById("blockLite").classList.remove("disabled")
